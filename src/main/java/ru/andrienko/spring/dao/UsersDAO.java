32dd6f839df9105cd4ec0693f13eca1b583dcc3c
Anfisa Andrienko <anfisa.andrienko.aa@gmail.com>
2020-12-16 14:34:20 +0300

    Лобавила методы взаимодействия с данными

--- src/main/java/ru/andrienko/spring/dao/UsersDAO.java
+++ src/main/java/ru/andrienko/spring/dao/UsersDAO.java
@@ -1,4 +1,69 @@
 package ru.andrienko.spring.dao;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import ru.andrienko.spring.config.HibernateConfig;
+import ru.andrienko.spring.models.User;
+
+import java.util.List;
+
+@Repository
 public class UsersDAO {
+    private Transaction transaction;
+
+    private UsersDAO() {
+    }
+
+    public User getUser(long id){
+        Session session =  HibernateConfig.getInstance().openSession();
+        User user = (User) session.get(User.class, id);
+        session.close();
+        return user;
+    }
+
+    public User getUser(String login){
+        Session session = HibernateConfig.getInstance().openSession();
+        Query query = session.createQuery("FROM User WHERE login='" + login + "'");
+        User user = (User) query.list().get(0);
+        session.close();
+        return user;
+    }
+
+
+    public long insertUser(User user){
+        Session session =  HibernateConfig.getInstance().openSession();
+        transaction = session.beginTransaction();
+        long id = (Long) session.save(user);
+        transaction.commit();
+        session.close();
+        return id;
+    }
+
+    public void editUser(User user){
+        Session session = HibernateConfig.getInstance().openSession();
+        session.beginTransaction();
+        session.update(user);
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public List<User> getUsers(){
+        Session session = HibernateConfig.getInstance().openSession();
+        Query query = session.createQuery("FROM User ORDER BY id ASC");
+        List<User> users = query.list();
+        session.close();
+        return users;
+    }
+
+    public void deleteUser(long id){
+        Session session = HibernateConfig.getInstance().openSession();
+        session.beginTransaction();
+        session.createQuery("DELETE FROM User WHERE id=" + id).executeUpdate();
+        session.getTransaction().commit();
+        session.close();
+    }
 }
